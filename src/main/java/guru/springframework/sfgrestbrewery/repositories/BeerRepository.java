9e746facf044c43526782901bbc156d3aa26e5a4
Hweemyoung <legolass93@naver.com>
2023-08-01 15:40:04 +0900

    updated

--- src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
+++ src/main/java/guru/springframework/sfgrestbrewery/repositories/BeerRepository.java
@@ -6,12 +6,20 @@ import guru.springframework.sfgrestbrewery.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
-
+@RepositoryRestResource(
+        path = "beer", // Routes to {base-path}/beer
+        collectionResourceRel = "beer" // Responds entities like "beer": [{...}, ...]
+)
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
-    Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
+    /*
+    * Can call repository methods by:
+    * {endpoint}/search/{repository method}?{query params}
+    * */
+    Page<Beer> findAllByBeerName(String beerName, Pageable pageable); // /api/v1/beer/search/findAllByBeerName?beerName=foo
 
     Page<Beer> findAllByBeerStyle(BeerStyleEnum beerStyle, Pageable pageable);
 
